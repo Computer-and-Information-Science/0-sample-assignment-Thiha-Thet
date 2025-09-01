ba2d349fcbe43f00d6e91838f2304ed378b7fd04
Thiha-Thet <thiha.thet@student.northampton.edu>
2025-09-01 02:40:52 +0000

    completed assignment

--- sample.cpp
+++ sample.cpp
@@ -1,7 +1,14 @@
-// YOUR NAME HERE
+// Thiha Thet
 #include <iostream>
 using namespace std;
 
 int main () {
 
+    int first_integer;
+    int sec_integer;
+    cout << "Enter first integer: " << endl;
+    cin >> first_integer;
+    cout << "Enter second integer: " << endl;
+    cin >> sec_integer;
+    cout << "The sum is " << first_integer+sec_integer << "." << endl;
 }
